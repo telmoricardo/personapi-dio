90184b59bb5639ab0db6effccac66cc0bbbf367a
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-10-27 11:08:24 -0300

    criando personrepository e metodo create person

---
+++ src/main/java/one/digitalinovation/personapi/dto/MessagemResponseDTO.java
@@ -0,0 +1,2 @@
+package one.digitalinovation.personapi.dto;public class MessagemResponseDTO {
+}
---
+++ src/main/java/one/digitalinovation/personapi/repository/PersonRepository.java
@@ -0,0 +1,2 @@
+package one.digitalinovation.personapi.repository;public interface PersonRepository {
+}
