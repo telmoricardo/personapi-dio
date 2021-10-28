24988b60c6c52c6ee2a59e1250bbf0519bbceebb
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-10-28 18:14:59 -0300

    Develop delete person by id

--- src/main/java/one/digitalinovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinovation/personapi/service/PersonService.java
@@ -44,14 +44,17 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        //Optional<Person> optionalPerson = personRepository.findById(id);
-
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
-
-        /*if(optionalPerson.isEmpty()){
-            throw new PersonNotFoundException(id);
-        }*/
+        Person person = verifyIfExists(id);
         return personMapper.toDTO(person);
+    }
+
+    public void deleteById(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws   PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
